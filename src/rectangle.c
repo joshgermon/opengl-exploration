cd735485241371eb841a94b4165932cbd4f675a3
joshua <joshgermon@gmail.com>
2024-05-15 22:27:25 +1000

    Render rectangle with element buffer object

--- src/rectangle.c
+++ src/rectangle.c
@@ -2,20 +2,21 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
-const char *vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderSource =
+    "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
-const char *fragmentShaderSource = "#version 330 core\n"
+const char *fragmentShaderSource =
+    "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n\0";
-
 
 /* Callback to resize viewport on window resize */
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
@@ -40,10 +41,10 @@ void check_shader_compile(unsigned int shader) {
 void check_shader_prog_link(unsigned int shaderProgram) {
   int success;
   glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-  if(!success) {
-      char infoLog[512];
-      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-      printf("ERROR::SHADER::PROGRAM::LINK\n");
+  if (!success) {
+    char infoLog[512];
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    printf("ERROR::SHADER::PROGRAM::LINK\n");
   }
 }
 
@@ -73,7 +74,6 @@ int main(void) {
   glViewport(0, 0, 800, 600);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-
   /* ** SHADER COMPLIATION ** */
   /* Create and compile vertex shader */
   unsigned int vertexShader;
@@ -91,7 +91,7 @@ int main(void) {
   glCompileShader(fragmentShader);
   check_shader_compile(fragmentShader);
 
- /* Shader Program */
+  /* Shader Program */
   unsigned int shaderProgram;
   shaderProgram = glCreateProgram();
 
@@ -108,7 +108,16 @@ int main(void) {
   glDeleteShader(fragmentShader);
 
   /* ** VERTEX DATA  ** */
-  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  float vertices[] = {
+       0.5f,  0.5f, 0.0f,  // top right
+       0.5f, -0.5f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f,  // bottom left
+      -0.5f,  0.5f, 0.0f   // top left
+  };
+  unsigned int indices[] = {  // note that we start from 0!
+      0, 1, 3,   // first triangle
+      1, 2, 3    // second triangle
+  };
 
   /* Setup vertex array object */
   unsigned int VAO;
@@ -121,11 +130,21 @@ int main(void) {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  /* Setup element array object to hold indices */
+  unsigned int EAO;
+  glGenBuffers(1, &EAO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EAO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
   glEnableVertexAttribArray(0);
 
   /* Unbind VBO and VAO */
+
+  /* You can unbind the VBO, the call to glVertexAttribPointer registered VBO as the
+   * vertex attribute's bound vertex buffer object so afterwards we can safely unbind */
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+  /* DON'T unbind element buffer obj as it will remove the bind inside the VAO */
   glBindVertexArray(0);
 
   /* Loop until the user closes the window */
@@ -139,7 +158,9 @@ int main(void) {
     /* Draw */
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
